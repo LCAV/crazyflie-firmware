6e28cee0bc02135638604e291cd7ad6437a28790
Isaac Dinis <isaac.dinis@epfl.ch>
2020-08-18 17:05:48 +0200

    added comments

--- src/deck/drivers/src/audio_deck.c
+++ src/deck/drivers/src/audio_deck.c
@@ -21,10 +21,12 @@
 
 
 #define AUDIO_DECK_ADDRESS 47
+#define AUDIO_TASK_FREQUENCY 100 // frequency at each packets are sent
 #define CRTP_MAX_PAYLOAD 29
 #define FFT_SIZE 32
 #define N_MICS 4
-#define ARRAY_SIZE N_MICS*FFT_SIZE*4*2
+#define FLOAT_PRECISION 4
+#define ARRAY_SIZE N_MICS*FFT_SIZE*FLOAT_PRECISION*2  // *2 for complex numbers
 #define NPACKETS (int)ARRAY_SIZE/CRTP_MAX_PAYLOAD
 
 static BYTE corr_array[ARRAY_SIZE];
@@ -32,20 +34,11 @@ static bool isInit;
 
 static bool corr_matrix_sending = 0;
 
-
-
 ////////////////////////////////////// AUDIO DECK FUNCTIONS /////////////////////////////////
 
 void fillbuffer(uint8_t buffer[],uint8_t packet_count, uint8_t size){
 	for(uint8_t i = 0; i<size; i++)
 		buffer[i]=corr_array[packet_count*CRTP_MAX_PAYLOAD+i];
-}
-
-void con_64_bit_to_8_bit_array(uint64_t data, uint8_t array[]){
-	for (uint16_t i = 7;i>=0;i--){
-		array[i] = data&0xFF;
-		data >>= 8;
-	}
 }
 
 void send_corr_packet(uint8_t channel){
@@ -55,13 +48,13 @@ void send_corr_packet(uint8_t channel){
 	corr_array_p.header = CRTP_HEADER(CRTP_PORT_AUDIO, channel);
 	corr_array_p.size = CRTP_MAX_PAYLOAD;
 
-	if (packet_count == NPACKETS){
+	if (packet_count == NPACKETS){ 	// send last packet and reset counter
 		fillbuffer(corr_array_p.data,packet_count,ARRAY_SIZE%CRTP_MAX_PAYLOAD);
 		crtpSendPacket(&corr_array_p);
 		corr_matrix_sending = 0;
 		packet_count = 0;
 	}
-	else{
+	else{ 							// send packet
 		fillbuffer(corr_array_p.data,packet_count,CRTP_MAX_PAYLOAD);
 		crtpSendPacket(&corr_array_p);
 		packet_count++;
@@ -95,8 +88,8 @@ void audio_deckTask(void* arg){
   while (1) {
     vTaskDelayUntil(&xLastWakeTime, F2T(100));
     if (!corr_matrix_sending){
-    	i2cdevRead(I2C1_DEV, AUDIO_DECK_ADDRESS, ARRAY_SIZE, corr_array);
-    	send_corr_packet(1);
+    	i2cdevRead(I2C1_DEV, AUDIO_DECK_ADDRESS, ARRAY_SIZE, corr_array); // get array from deck
+    	send_corr_packet(1); // first packet is sent in channel 1 (start condition)
     	corr_matrix_sending = 1;
     }
     else{
