6186f151d9db09cb41d43104196580295886129d
duembgen <frederike.duembgen@epfl.ch>
2020-08-31 16:39:11 +0200

    Renaming (compiles but breaks)

--- src/deck/drivers/src/audio_deck.c
+++ src/deck/drivers/src/audio_deck.c
@@ -21,31 +21,35 @@
 
 ////////////////////////////////////// DEFINES /////////////////////////////////
 
-#define AUDIO_DECK_ADDRESS 47    // I2C adress of the deck
-#define AUDIO_TASK_FREQUENCY 100 // frequency at which packets are sent [Hz]
+// constants
+#define INT16_PRECISION 2 // int16 = 2 bytes
+#define FLOAT_PRECISION 4// float32 = 4 bytes
 #define CRTP_MAX_PAYLOAD 29
+
+// audio deck parameters
 #define FFTSIZE 32
 #define N_MICS 4
-#define FLOAT_PRECISION 4  // float32 = 4 bytes
-#define FLOAT_ARRAY_SIZE N_MICS*FFTSIZE*2 // *2 for complex numbers
-#define BYTE_ARRAY_SIZE FLOAT_ARRAY_SIZE*FLOAT_PRECISION
-#define N_FULL_PACKETS (int)BYTE_ARRAY_SIZE/CRTP_MAX_PAYLOAD
-#define N_PACKETS (N_FULL_PACKETS + 1)
-
-#define FBINS_ARRAY_SIZE_BYTES FFTSIZE * INT16_PRECISION
-#define N_FULL_PACKETS_FBINS (int)FBINS_ARRAY_SIZE_BYTES/CRTP_MAX_PAYLOAD
-#define N_PACKETS_FBINS (N_FULL_PACKETS_FBINS+1)
-#define BYTE_ARRAY_SIZE_TOT (FBINS_ARRAY_SIZE_BYTES+BYTE_ARRAY_SIZE)
-
-#define I2C_REQUEST_RATE 6 // I2C is requested each 6 cycles of main task i.e. 60 ms = 6/100Hz
-#define M 4 // number of arrays averaged, maximum is 6 as 6*6 = 36 = N_PACKETS
-#define USE_IIR 0 // put to one to use exp filter instead of averaging through M buffers
-#define IIR_ALPHA 0.5f
+#define AUDIO_DECK_ADDRESS 47// I2C adress of the deck
 #define N_MOTORS 4
-#define INT16_PRECISION 2 // int16 = 2 bytes
-#define SIZE_OF_PARAM_I2C 3 // in uint16, min_freq = 1, max_freq = 1, snr + propeller enable = 1, tot = 3
-#define I2C_SEND_LENGTH_INT16 (N_MOTORS+SIZE_OF_PARAM_I2C)
-#define I2C_SEND_LENGTH_BYTE I2C_SEND_LENGTH_INT16 * INT16_PRECISION
+#define AUDIO_TASK_FREQUENCY 100 // frequency at which packets are sent [Hz]
+#define I2C_REQUEST_RATE 6 // I2C is requested each 6 cycles of main task i.e. 60 ms = 6/100Hz
+#define SIZE_OF_PARAM_I2C 3 // in uint16, min_freq = 1, max_freq = 1, snr + propeller enable = 1
+
+// buffer sizes
+#define PARAM_N_INTS (N_MOTORS + SIZE_OF_PARAM_I2C) // in uint16, n_motors for the current trhust commands
+#define PARAM_N_BYTES (PARAM_N_INTS * INT16_PRECISION)
+
+#define AUDIO_N_FLOATS N_MICS * FFTSIZE * 2 // *2 for complex numbers
+#define AUDIO_N_BYTES (AUDIO_N_FLOATS * FLOAT_PRECISION)
+#define AUDIO_N_PACKETS_FULL (int) AUDIO_N_BYTES / CRTP_MAX_PAYLOAD
+#define AUDIO_N_PACKETS (AUDIO_N_PACKETS_FULL + 1)
+
+#define FBINS_N_BYTES FFTSIZE * INT16_PRECISION
+#define FBINS_N_PACKETS_FULL (int) FBINS_N_BYTES / CRTP_MAX_PAYLOAD
+#define FBINS_N_PACKETS (FBINS_N_PACKETS_FULL + 1)
+
+#define TOTAL_N_BYTES (FBINS_N_BYTES + AUDIO_N_BYTES)
+
 ////////////////////////////////////// PRIVATE VARIABLES /////////////////////////////////
 static enum{
   SEND_FIRST_PACKET,
@@ -54,156 +58,171 @@ static enum{
 } state = SEND_FIRST_PACKET;
 
 
-static BYTE byte_array_CRTP[BYTE_ARRAY_SIZE];     // buffer to be sent through CRTP after averaging
-static BYTE byte_array_received[BYTE_ARRAY_SIZE_TOT]; // buffer where I2C data is received
-static float float_array_averaged[FLOAT_ARRAY_SIZE]; // buffer where I2C data is averaged
-static float float_array_buffer[FLOAT_ARRAY_SIZE];   // buffer where I2C data is converted to float
+static BYTE byte_array_CRTP[AUDIO_N_BYTES]; // buffer to be sent through CRTP
+static BYTE byte_array_received[TOTAL_N_BYTES]; // buffer where I2C data is received
+static float float_array_averaged[AUDIO_N_FLOATS]; // buffer where I2C data is averaged
+static float float_array_buffer[AUDIO_N_FLOATS]; // buffer where I2C data is converted to float
 
-static uint16_t I2C_send_packet_int16[I2C_SEND_LENGTH_INT16];
+static uint16_t I2C_send_packet_int16[PARAM_N_INTS]; // buffer with the current parameters
 
 static bool isInit;
 
-static uint8_t packet_count = 0;
+static uint8_t packet_count_audio = 0;
 static uint8_t packet_count_fbins = 0;
 
+// general parameter
 static bool send_audio_enable = 0; // enables the sending of CRTP packets with the audio data
+
+// frequency selection parameters
 static bool filter_propellers_enable = 1;
 static bool filter_snr_enable = 0;
 static uint16_t min_freq = 200;
 static uint16_t max_freq = 10000;
 
+// averaging parameters
+static bool use_iir = 0; // put to one to use IIR filter instead of averaging through moving_average_window buffers
+static float alpha_iir = 0.5; // iir moving average parameter
+static uint8_t moving_average_window = 4; // number of arrays averaged, maximum is 6 as 6*6 = 36 = AUDIO_N_PACKETS
+
 ////////////////////////////////////// AUDIO DECK FUNCTIONS /////////////////////////////////
 
 void byte_array_to_float(uint8_t input[], float* output)
 {
-  for (int i = 0;i<FLOAT_PRECISION;i++){
-    *((uint8_t*)(output) + i) = input[i];
-    state = SEND_AUDIO_PACKET;
+  for (int i = 0; i < FLOAT_PRECISION; i++){
+      *((uint8_t*)(output) + i) = input[i];
+      // TODO(FD) this looks misplaced.
+      state = SEND_AUDIO_PACKET;
   }
 }
 
 void float_to_byte_array(float input, uint8_t output[]){
   uint32_t temp = *((uint32_t*) &input);
-  for (int i = 0;i<FLOAT_PRECISION;i++){
-    output[i] = temp&0xFF;
-    temp >>= 8;
+  for (int i = 0; i < FLOAT_PRECISION; i++){
+      output[i] = temp&0xFF;
+      temp >>= 8;
   }
 }
 
-void fillbuffer(uint8_t buffer[],uint8_t packet_count, uint8_t size){
-	for(uint8_t i = 0; i<size; i++)
-		buffer[i]=byte_array_CRTP[packet_count*CRTP_MAX_PAYLOAD+i];
+void fill_packet_data_fbins(uint8_t buffer[],uint8_t packet_count, uint8_t size){
+  for(uint8_t i = 0; i < size; i++)
+    buffer[i] = byte_array_received[packet_count * CRTP_MAX_PAYLOAD + i];
 }
 
-void fillbuffer_fbins(uint8_t buffer[],uint8_t packet_count, uint8_t size){
-  for(uint8_t i = 0; i<size; i++)
-    buffer[i]=byte_array_received[packet_count*CRTP_MAX_PAYLOAD+i];
-}
-
-void byte_array_to_float_array(float float_array[],uint8_t byte_array[]){
-  for (int i = 0; i<FLOAT_ARRAY_SIZE; i++){
-    byte_array_to_float(&byte_array[i*FLOAT_PRECISION],&float_array[i]);
+/**
+ *  Fill a new packet with the corresponding data from byte_array_CRTP.
+ */
+void fill_packet_data(uint8_t packet_data[], uint8_t packet_count, uint8_t packet_size){
+  for(uint8_t i = 0; i < packet_size; i++) {
+      packet_data[i] = byte_array_CRTP[packet_count*CRTP_MAX_PAYLOAD+i];
   }
 }
 
-void float_array_to_byte_array(float float_array[],uint8_t byte_array[]){
-  for (int i = 0; i<FLOAT_ARRAY_SIZE; i++){
-    float_to_byte_array(float_array[i],&byte_array[i*FLOAT_PRECISION]);
+void byte_array_to_float_array(float float_array[], uint8_t byte_array[], uint16_t n_floats){
+  for (int i = 0; i < n_floats; i++){
+      byte_array_to_float(&byte_array[i*FLOAT_PRECISION], &float_array[i]);
   }
 }
 
-void add_divided_array_to_buffer(float array_to_divide[],float buffer[]){
-  for (int i = 0; i<FLOAT_ARRAY_SIZE; i++){
-    buffer[i]+=array_to_divide[i]/M;
-    //buffer[i]=array_to_divide[i];
+void float_array_to_byte_array(float float_array[], uint8_t byte_array[]){
+  for (int i = 0; i < AUDIO_N_FLOATS; i++){
+      float_to_byte_array(float_array[i], &byte_array[i * FLOAT_PRECISION]);
   }
 }
 
-void exp_filter(float incoming_buffer[],float buffer[]){
-  for (int i = 0; i<FFTSIZE*N_MICS*2; i++){
-    buffer[i]=buffer[i]*(1.0f-IIR_ALPHA)+incoming_buffer[i]*IIR_ALPHA;
+void add_divided_array_to_buffer(float array_to_divide[], float buffer[]){
+  for (int i = 0; i < AUDIO_N_FLOATS; i++){
+      buffer[i] += array_to_divide[i] / moving_average_window;
   }
 }
 
-void erase_buffer(float buffer[],int buffer_size){
+void exp_filter(float incoming_buffer[], float buffer[]){
+  for (int i = 0; i < AUDIO_N_FLOATS; i++){
+      buffer[i]=buffer[i] * (1.0f-alpha_iir)+incoming_buffer[i]*alpha_iir;
+  }
+}
+
+void erase_buffer(float buffer[], int buffer_size){
   for (int i = 0; i<buffer_size; i++){
-    buffer[i] = 0;
+      buffer[i] = 0;
   }
 }
 
-
-void copy_buffer(uint16_t input[],uint16_t output[],uint8_t buffer_size){
+void copy_buffer(uint16_t input[], uint16_t output[], uint8_t buffer_size){
   for (uint8_t i = 0; i<buffer_size; i++){
-    output[i] = input[i];
+      output[i] = input[i];
   }
 }
 
-void send_array_packet(uint8_t channel){
+void send_audio_packet(uint8_t channel){
   if(send_audio_enable){
-  	static CRTPPacket corr_array_p;
-  	corr_array_p.header = CRTP_HEADER(CRTP_PORT_AUDIO, channel);
-  	corr_array_p.size = CRTP_MAX_PAYLOAD;
+      static CRTPPacket signal_array_p;
+      signal_array_p.header = CRTP_HEADER(CRTP_PORT_AUDIO, channel);
+      signal_array_p.size = CRTP_MAX_PAYLOAD;
 
-  	if (packet_count == N_FULL_PACKETS){ 	// send last packet and reset counter
-  		fillbuffer(corr_array_p.data,packet_count,BYTE_ARRAY_SIZE%CRTP_MAX_PAYLOAD);
-  		crtpSendPacket(&corr_array_p);
-  		state = SEND_FBIN_PACKET;
-  		packet_count = 0;
-  	}
-  	else{  // send packet
-  		fillbuffer(corr_array_p.data,packet_count,CRTP_MAX_PAYLOAD);
-  		crtpSendPacket(&corr_array_p);
-  		packet_count++;
-  	}
+      if (packet_count_audio == AUDIO_N_PACKETS_FULL){ 	// send last packet and reset counter
+          fill_packet_data(signal_array_p.data, packet_count_audio, AUDIO_N_BYTES % CRTP_MAX_PAYLOAD);
+          crtpSendPacket(&signal_array_p);
+          state = SEND_FBIN_PACKET;
+          packet_count_audio = 0;
+      }
+      else { // send full packet
+          fill_packet_data(signal_array_p.data, packet_count_audio, CRTP_MAX_PAYLOAD);
+          crtpSendPacket(&signal_array_p);
+          packet_count_audio++;
+      }
   }
 }
 
 void send_fbin_packet(){
   if(send_audio_enable){
-    static CRTPPacket fbin_array_p;
-    fbin_array_p.header = CRTP_HEADER(CRTP_PORT_AUDIO, 2);
-    fbin_array_p.size = CRTP_MAX_PAYLOAD;
+      static CRTPPacket fbin_array_p;
+      fbin_array_p.header = CRTP_HEADER(CRTP_PORT_AUDIO, 2);
+      fbin_array_p.size = CRTP_MAX_PAYLOAD;
 
-    if (packet_count_fbins == N_FULL_PACKETS_FBINS){  // send last packet and reset counter
-      fillbuffer_fbins(fbin_array_p.data,N_PACKETS+packet_count_fbins,FBINS_ARRAY_SIZE_BYTES%CRTP_MAX_PAYLOAD);
-      crtpSendPacket(&fbin_array_p);
-      state = SEND_FIRST_PACKET;
-      packet_count_fbins = 0;
-    }
-    else{  // send packet
-      fillbuffer(fbin_array_p.data,N_PACKETS+packet_count_fbins,CRTP_MAX_PAYLOAD);
-      crtpSendPacket(&fbin_array_p);
-      packet_count_fbins++;
-    }
+      if (packet_count_fbins == FBINS_N_PACKETS_FULL){// send last packet and reset counter
+          //fill_packet_data(fbin_array_p.data, AUDIO_N_PACKETS + packet_count_fbins, FBINS_N_BYTES % CRTP_MAX_PAYLOAD);
+          fill_packet_data_fbins(fbin_array_p.data, AUDIO_N_PACKETS + packet_count_fbins, FBINS_N_BYTES % CRTP_MAX_PAYLOAD);
+          crtpSendPacket(&fbin_array_p);
+          state = SEND_FIRST_PACKET;
+          packet_count_fbins = 0;
+      }
+      else{// send full packet
+          fill_packet_data(fbin_array_p.data, AUDIO_N_PACKETS + packet_count_fbins, CRTP_MAX_PAYLOAD);
+          crtpSendPacket(&fbin_array_p);
+          packet_count_fbins++;
+      }
   }
 }
 
+/** Request current audio data from the audio deck (signals and frequency bins)
+ *
+ */
 void receive_audio_deck_array(){
-  i2cdevRead(I2C1_DEV, AUDIO_DECK_ADDRESS, BYTE_ARRAY_SIZE_TOT, byte_array_received);
-  byte_array_to_float_array(float_array_buffer,byte_array_received);
-  if (!USE_IIR){
-    add_divided_array_to_buffer(float_array_buffer,float_array_averaged);  
+  i2cdevRead(I2C1_DEV, AUDIO_DECK_ADDRESS, TOTAL_N_BYTES, byte_array_received);
+  byte_array_to_float_array(float_array_buffer, byte_array_received, AUDIO_N_FLOATS);
+  if (!use_iir) {
+      add_divided_array_to_buffer(float_array_buffer, float_array_averaged);
   }
   else{
-    exp_filter(float_array_buffer,float_array_averaged); 
-  }     
+      exp_filter(float_array_buffer, float_array_averaged);
+  }
 }
 
 void send_param_I2C(){
   uint16_t *motorPower_p = get_motor_power();
-  copy_buffer(motorPower_p,I2C_send_packet_int16,N_MOTORS);
+  copy_buffer(motorPower_p, I2C_send_packet_int16, N_MOTORS);
 
-  I2C_send_packet_int16[4] = min_freq;
-  I2C_send_packet_int16[5] = max_freq;
+  I2C_send_packet_int16[N_MOTORS] = min_freq;
+  I2C_send_packet_int16[N_MOTORS + 1] = max_freq;
 
   uint16_t enables = (filter_propellers_enable << 8) | filter_snr_enable;
-  I2C_send_packet_int16[6] = enables;
+  I2C_send_packet_int16[N_MOTORS + 2] = enables;
 
   uint8_t *I2C_send_packet_byte = (uint8_t*)I2C_send_packet_int16;
-  i2cdevWrite(I2C1_DEV, AUDIO_DECK_ADDRESS,I2C_SEND_LENGTH_BYTE,I2C_send_packet_byte);
+  i2cdevWrite(I2C1_DEV, AUDIO_DECK_ADDRESS, PARAM_N_BYTES, I2C_send_packet_byte);
 }
 
-void send_motorPower(){ // not used because with I2C parameters
+void send_motorPower(){ // not used anymore because included in send_param_I2C
   uint16_t *motorPower_p;
   motorPower_p = get_motor_power();
   uint8_t *motorPower_byte_p = (uint8_t*)motorPower_p;
@@ -219,8 +238,7 @@ void audio_deckInit(DeckInfo* info){ // deck initialisation
   isInit = true;
 }
 
-
-bool audio_deckTest(void){  // deck test
+bool audio_deckTest(void){// deck test
   if (!isInit)
     return false;
   DEBUG_PRINT("AUDIO TEST_PASSED");
@@ -231,32 +249,33 @@ void audio_deckTask(void* arg){ // main task
   systemWaitStart();
   TickType_t xLastWakeTime;
   xLastWakeTime = xTaskGetTickCount();
-  if (USE_IIR){
-    i2cdevRead(I2C1_DEV, AUDIO_DECK_ADDRESS, BYTE_ARRAY_SIZE_TOT, byte_array_received); // get array from deck for initialization
-    byte_array_to_float_array(float_array_averaged,byte_array_received);
+  if (use_iir){
+      i2cdevRead(I2C1_DEV, AUDIO_DECK_ADDRESS, TOTAL_N_BYTES, byte_array_received); // get array from deck for initialization
+      byte_array_to_float_array(float_array_averaged, byte_array_received, AUDIO_N_FLOATS);
   }
   while (1) {
-    vTaskDelayUntil(&xLastWakeTime, F2T(AUDIO_TASK_FREQUENCY));
+      vTaskDelayUntil(&xLastWakeTime, F2T(AUDIO_TASK_FREQUENCY));
+      if (state == SEND_FIRST_PACKET){
+          float_array_to_byte_array(float_array_averaged, byte_array_CRTP); // transfer the averaged audio
 
-    if (state == SEND_FIRST_PACKET){
-      float_array_to_byte_array(float_array_averaged,byte_array_CRTP); // transfer the averaged array to the buffer to be sent
-      if(!USE_IIR){
-        erase_buffer(float_array_averaged,FLOAT_ARRAY_SIZE);
+          if(!use_iir){
+              erase_buffer(float_array_averaged, AUDIO_N_FLOATS);
+          }
+          send_audio_packet(1); // first packet is sent in channel 1 (start condition)
+          send_param_I2C(); // send parameters to the audio deck with I2C
+          state = SEND_AUDIO_PACKET;
       }
-   	  send_array_packet(1); // first packet is sent in channel 1 (start condition)
-      send_param_I2C(); // send parameters to the audio deck with I2C
-      state = SEND_AUDIO_PACKET;
-    }
-    else if(state == SEND_AUDIO_PACKET){
-      if (packet_count%I2C_REQUEST_RATE == 0 && (packet_count >= N_PACKETS-I2C_REQUEST_RATE*M || USE_IIR)){
-        // we average M arrays before sending or use exp filter, must be separated with I2C_REQUEST_RATE cycles
-        receive_audio_deck_array();
+      else if(state == SEND_AUDIO_PACKET){
+          if ((packet_count_audio % I2C_REQUEST_RATE == 0) &&
+              (packet_count_audio >= AUDIO_N_PACKETS - I2C_REQUEST_RATE * moving_average_window || use_iir)){
+              // we average before sending, and calls are separated with I2C_REQUEST_RATE cycles
+              receive_audio_deck_array();
+          }
+          send_audio_packet(0);
       }
-   	  send_array_packet(0);
-    }
-    else if (state == SEND_FBIN_PACKET){
-      send_fbin_packet();
-    }
+      else if (state == SEND_FBIN_PACKET){
+          send_fbin_packet();
+      }
   }
 }
 
@@ -272,6 +291,9 @@ static const DeckDriver audio_deck = {
 DECK_DRIVER(audio_deck);
 
 PARAM_GROUP_START(audio)
+PARAM_ADD(PARAM_INT8, use_iir, &use_iir)
+PARAM_ADD(PARAM_FLOAT, alpha_iir, &alpha_iir)
+PARAM_ADD(PARAM_INT8, moving_average_window, &moving_average_window)
 PARAM_ADD(PARAM_INT8, send_audio_enable, &send_audio_enable)
 PARAM_ADD(PARAM_INT8, filter_prop_enable, &filter_propellers_enable)
 PARAM_ADD(PARAM_INT8, filter_snr_enable, &filter_snr_enable)
