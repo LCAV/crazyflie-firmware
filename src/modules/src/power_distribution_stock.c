133e9e4c52a3b20d940043fb40cb63355f21f858
Adrien Hoffet <adrien.hoffet@epfl.ch>
2020-10-20 10:53:38 +0200

    change get motor power function

--- src/modules/src/power_distribution_stock.c
+++ src/modules/src/power_distribution_stock.c
@@ -117,10 +117,30 @@ void powerDistribution(const control_t *control)
 // in the firmware than below version.
 uint16_t * get_motor_power(void){
     static uint16_t motorPower_array[4];
+
+// TODO (AH): Test this version and remove previous TODO
+#if 0
     motorPower_array[0] = motorPowerSet.m1;
     motorPower_array[1] = motorPowerSet.m2;
     motorPower_array[2] = motorPowerSet.m3;
     motorPower_array[3] = motorPowerSet.m4;
+#else
+    if (motorSetEnable)
+    {
+        motorPower_array[0] = motorPowerSet.m1;
+        motorPower_array[1] = motorPowerSet.m2;
+        motorPower_array[2] = motorPowerSet.m3;
+        motorPower_array[3] = motorPowerSet.m4;
+    }
+    else
+    {
+        motorPower_array[0] = motorPower.m1;
+        motorPower_array[1] = motorPower.m2;
+        motorPower_array[2] = motorPower.m3;
+        motorPower_array[3] = motorPower.m4;
+    }
+#endif
+
     return motorPower_array;
 }
 PARAM_GROUP_START(motorPowerSet)
